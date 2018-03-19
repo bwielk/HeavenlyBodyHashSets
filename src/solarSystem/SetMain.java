69ad94e51678ade46e6feb0cfb3fae99a0a7b543
bwielk <bwielk@gmail.com>
2018-03-19 23:02:32 +0100

    Implementing the addAll() and retainAll() methods

--- src/solarSystem/SetMain.java
+++ src/solarSystem/SetMain.java
@@ -13,5 +13,17 @@ public class SetMain {
 		}
 		
 		System.out.println("There are " + squares.size() + " squares and " + cubes.size() + " cubes.");
+		
+		//union
+		Set<Integer> union = new HashSet<>(squares);
+		union.addAll(cubes);
+		System.out.println("union Set contains " + union.size() + " elements");//196 elements
+		//intersection
+		Set<Integer> intersection = new HashSet<>(squares);
+		intersection.retainAll(cubes);
+		System.out.println("intersection Set contains " + intersection.size() + " elements");//4 elements
+		for(int i : intersection){
+			System.out.println("\t" + i + " is the square of " + Math.sqrt(i) + " and the cube of " + Math.cbrt(i));
+		}
 	}
 }
