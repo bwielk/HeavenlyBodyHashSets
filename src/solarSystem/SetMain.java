4c8d553cbe6f7b6cd7aa4026d20a30f15d26aff9
bwielk <bwielk@gmail.com>
2018-03-19 22:50:19 +0100

    Added a new class SetMain. Practicing bulk methods on Sets

---
+++ src/solarSystem/SetMain.java
@@ -0,0 +1,17 @@
+package solarSystem;
+
+import java.util.*;
+
+public class SetMain {
+	public static void main(String[] args){
+		Set<Integer> squares = new HashSet<>();
+		Set<Integer> cubes = new HashSet<>();
+		
+		for(int i=1; i<=100; i++){
+			squares.add(i*i);
+			cubes.add(i*i*i);
+		}
+		
+		System.out.println("There are " + squares.size() + " squares and " + cubes.size() + " cubes.");
+	}
+}
